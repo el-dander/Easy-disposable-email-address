395000829703019ab31cc3aca6847cec3339f9da
El Dander <el-dander@local>
2012-03-09 22:03:33 +0000

    Move javascript out of html

---
+++ src/background.js
@@ -0,0 +1,60 @@
+function randomString(string_length) {
+  var chars = "0123456789abcdefghiklmnopqrstuvwxyz";
+  var randomstring = '';
+
+  for (var i=0; i<string_length; i++) {
+    var rnum = Math.floor(Math.random() * chars.length);
+    randomstring += chars.substring(rnum,rnum+1);
+  }
+
+  return randomstring;
+}
+
+function getAddress(provider, curIndex)
+{
+  // Generate random address
+  var local_part = randomString(8);
+  var domain = provider["domain"];
+  var inbox_url = provider["inbox_url"] + local_part;
+  var email_address = local_part + "@" + domain;
+ 
+  // Check chosen site is working, if not return false
+  var xhReq = new XMLHttpRequest();
+  xhReq.open("GET", inbox_url, false);
+  xhReq.send(null);
+  if( xhReq.status != 200 )
+    return false;
+ 
+  // Inject address into textbox
+  var js = "if (document.activeElement != undefined) document.activeElement.value = '" + email_address + "'";
+  chrome.tabs.executeScript(null, {allFrames: true, code: js});
+  
+  // Load up inbox after current tab in background
+  chrome.tabs.create({ url: inbox_url, selected: false, index: curIndex+1 });
+
+  return true;
+}
+
+function genericOnClick(info, tab) {
+  var def = localStorage["provider"];
+  var res = false;
+  
+  // Try default provider
+  if( def )
+    res = getAddress(providers[def], tab["index"]);
+
+  // If no default, or default failed, try the others
+  if( !res )
+    for(var i in providers)
+      if( (res = getAddress(providers[i], tab["index"])) )
+        break;
+  
+  // Failure - none worked
+  if( !res )
+    chrome.tabs.executeScript(null, {
+      code: "alert('Unable to create disposable email address, no sites reachable')"
+    });
+}
+
+var id = chrome.contextMenus.create(
+  {"title": "Insert disposable address", "contexts":["editable"], "onclick": genericOnClick});
