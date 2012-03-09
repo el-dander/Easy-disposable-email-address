aff9fe4d79470859208ef04c3316e97f4dc6cbdc
El Dander <el-dander@local>
2012-03-09 22:10:41 +0000

    Remove non-working providers

--- src/providers.js
+++ src/providers.js
@@ -1,7 +1,5 @@
 var providers = [
   {name: "Dispostable", domain: "dispostable.com", inbox_url: "http://www.dispostable.com/inbox/"},
   {name: "mailinator", domain: "mailinator.com", inbox_url: "http://mailinator.com/maildir.jsp?email="},
-  {name: "mailcatch", domain: "mailcatch.com", inbox_url: "http://mailcatch.com/en/temporary-inbox?box="},
-  {name: "dodgit", domain: "dodgeit.com", inbox_url: "http://dodgit.com/run/checkmail?mailbox="},
-  {name: "disposeamail", domain: "disposeamail.com", inbox_url: "http://disposeamail.com/inbox/"}
+  {name: "mailcatch", domain: "mailcatch.com", inbox_url: "http://mailcatch.com/en/temporary-inbox?box="}
 ];
