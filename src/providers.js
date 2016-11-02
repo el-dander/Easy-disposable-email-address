5d1cbe9c672f1a4f83aaee202614103ded476aeb
shark0der <tolean.dj@gmail.com>
2016-11-02 20:02:28 +0200

    Fix mailinator provider URL

--- src/providers.js
+++ src/providers.js
@@ -1,6 +1,6 @@
 var providers = [
   {name: "Dispostable", domain: "dispostable.com", inbox_url: "http://www.dispostable.com/inbox/"},
-  {name: "mailinator", domain: "mailinator.com", inbox_url: "http://mailinator.com/maildir.jsp?email="},
+  {name: "mailinator", domain: "mailinator.com", inbox_url: "https://www.mailinator.com/inbox2.jsp?public_to="},
   {name: "mailcatch", domain: "mailcatch.com", inbox_url: "http://mailcatch.com/en/temporary-inbox?box="},
   {name: "Harakirimail", domain: "harakirimail.com", inbox_url: "https://harakirimail.com/inbox/"}
 ];
